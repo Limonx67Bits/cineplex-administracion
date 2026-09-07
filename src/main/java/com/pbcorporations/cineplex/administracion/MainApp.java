a9305c2731ff675c0d6d81cec9c1c75890e3af66
Limonx67Bits <lemmon2octv@gmail.com>
2026-09-06 23:39:19 -0600

    Login implementado con exito

--- src/main/java/com/pbcorporations/cineplex/administracion/MainApp.java
+++ src/main/java/com/pbcorporations/cineplex/administracion/MainApp.java
@@ -1,9 +1,21 @@
 package main.java.com.pbcorporations.cineplex.administracion;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import main.java.com.pbcorporations.cineplex.administracion.util.SceneManager;
 
+public class MainApp extends Application{
+    @Override
+    public void start(Stage primary){
+        try{
+            SceneManager manager = new SceneManager(primary);
+            manager.showLoginView();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
-        
+        launch(args);
     }
     
 }
--- src/main/java/com/pbcorporations/cineplex/administracion/controller/LoginController.java
+++ src/main/java/com/pbcorporations/cineplex/administracion/controller/LoginController.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.java.com.pbcorporations.cineplex.administracion.controller;
 
-/**
- *
- * @author Familia García
- */
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import main.java.com.pbcorporations.cineplex.administracion.model.pojo.Usuario;
+import main.java.com.pbcorporations.cineplex.administracion.model.service.AuthService;
+import main.java.com.pbcorporations.cineplex.administracion.util.SceneManager;
+
+public class LoginController implements Initializable {
+    SceneManager manager;
+    AuthService service;
     
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private PasswordField pwField;
+    
+    public LoginController(AuthService service, SceneManager manager){
+        this.service = service;
+        this.manager = manager;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+    public void handleLogin(){
+        if(txtUsername.getText().trim() == null || txtUsername.getText().isEmpty()
+                || pwField.getText().trim() == null || pwField.getText().isEmpty()){
+            manager.showAlertInfo("Inicio de Sesión incorrecto", "Iniciando Sesión...", "Revise los campos ingresados antes de iniciar sesión", Alert.AlertType.WARNING);
+        }
+        
+        Usuario usuario = service.auth(txtUsername.getText(), pwField.getText());
+        if(usuario != null){
+            try{
+                manager.showDashboardView();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            manager.showAlertInfo("Inicio de Sesión denegado", "Iniciando Sesión...", "Credenciales incorrectas", Alert.AlertType.ERROR);
+        }
+    }
+    
+    public void handleRegisterButton(){
+        try{
+            manager.showRegisterView();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
